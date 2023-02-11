1d6f4a357ae7c1759285c22f0375029b40c0497d
Yury <jokofan@rambler.ru>
2023-02-11 16:04:44 +0300

    сумма случайных чисел

---
+++ ex_3/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = new Random().Next(1,10); // 1 2 3 4... 9
+Console.WriteLine(number1);
+int number2 = new Random().Next (1,10);
+Console.WriteLine(number2);
+int result = number1+number2;
+Console.WriteLine(result);
